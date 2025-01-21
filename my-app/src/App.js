775246fa54277a91a7bec053f6eb18220ee7763b
kazuto ando <kazuto.ando@gmail.com>
2025-01-21 20:10:58 +0900

    react component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,8 +18,35 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* MyButton component */}
+      <MyButton />
+
     </div>
   );
 }
+// MyButton component
+function MyButton(){
+    return(
+        <button> Button </button>
+    );
+}
 
 export default App;
+
+// 2025/1/21 React公式サイトのコンポーネント
+// MyButtonのコンポーネント
+// function MyButton(){
+//     return(
+//         <button> I'm a button</button>
+//     );
+// }
+// MyButton を宣言したら、別のコンポーネント
+// export default function MyApp(){
+//     return(
+//         <div>
+//             <h1>Welcome to my app</h1>
+//             <MyButton />
+//         </div>
+//     );
+// }
